81c8752f97901935cd55c2b7d2437b008392f3bc
Holger Fuessler <holger@fuessler.eu>
2019-03-25 22:54:21 +0100

    adding comments to version.go

--- version.go
+++ version.go
@@ -1,5 +1,10 @@
 package main
 
+// VERSION is the git version to be set by the LDFLAGS switch
 var VERSION string
+
+// COMMIT is the git commit to be set by the LDFLAGS switch
 var COMMIT string
+
+// BRANCH is the git branch to be set by the LDFLAGS switch
 var BRANCH string
